a3c5db9215b2bfb4e4d6758620599bddf60290f0
Alex <73026276+alex28042@users.noreply.github.com>
2023-08-26 11:37:43 +0200

    adding user and address

---
+++ common/src/main/java/send/toyou/common/valueObjects/Address.java
@@ -0,0 +1,12 @@
+package send.toyou.common.valueObjects;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+public class Address {
+    private String street;
+}
