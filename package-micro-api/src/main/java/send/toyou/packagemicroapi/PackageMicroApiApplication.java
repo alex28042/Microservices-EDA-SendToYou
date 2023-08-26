f23d8523d273a1522917ff31adb8b109deef53d9
Alex <73026276+alex28042@users.noreply.github.com>
2023-08-26 11:02:58 +0200

    Update PackageMicroApiApplication.java

--- package-micro-api/src/main/java/send/toyou/packagemicroapi/PackageMicroApiApplication.java
+++ package-micro-api/src/main/java/send/toyou/packagemicroapi/PackageMicroApiApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PackageMicroApiApplication {
 
 	public static void main(String[] args) {
+
+
 		SpringApplication.run(PackageMicroApiApplication.class, args);
 	}
 
