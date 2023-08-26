1f25b6a7b6cb9429f6b7cdbe894fadd98733f4d8
Alex <73026276+alex28042@users.noreply.github.com>
2023-08-26 19:53:26 +0200

    Create PackageMicroApiApplicationTests.java

---
+++ package-micro-api/src/test/java/send/toyou/packagemicroapi/PackageMicroApiApplicationTests.java
@@ -0,0 +1,15 @@
+package send.toyou.packagemicroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PackageMicroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
