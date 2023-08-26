a43421e769473cfe6f388c46f0b7ec84ac213c5f
Alex <73026276+alex28042@users.noreply.github.com>
2023-08-26 10:19:46 +0200

    mail-manager

---
+++ mail-manager/src/test/java/send/toyou/mailmanager/MailManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package send.toyou.mailmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
