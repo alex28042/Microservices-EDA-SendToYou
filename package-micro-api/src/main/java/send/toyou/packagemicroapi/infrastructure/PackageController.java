e75dabbd2f77089834cf1744d3f9ba383b825fae
Alex <73026276+alex28042@users.noreply.github.com>
2023-08-26 13:49:50 +0200

    added save package

--- package-micro-api/src/main/java/send/toyou/packagemicroapi/infrastructure/PackageController.java
+++ package-micro-api/src/main/java/send/toyou/packagemicroapi/infrastructure/PackageController.java
@@ -1,9 +1,29 @@
 package send.toyou.packagemicroapi.infrastructure;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+import send.toyou.packagemicroapi.application.PackageServiceImpl;
+import send.toyou.packagemicroapi.domain.Package;
 
 @RestController
 @RequestMapping("/api/package")
 public class PackageController {
+    @Autowired
+    private PackageServiceImpl packageService;
+
+    @PostMapping
+    public Mono<ResponseEntity<Package>> savePackage(@RequestBody Package packageToCreate) {
+        return this.packageService.save(packageToCreate)
+                .flatMap(packageCreated -> {
+                    if (packageCreated.equals(packageToCreate)) {
+                        return Mono.just(new ResponseEntity<>(packageCreated, HttpStatus.CREATED));
+                    }
+
+                    return Mono.just(new ResponseEntity<>(packageCreated, HttpStatus.BAD_REQUEST));
+                });
+    }
 }
