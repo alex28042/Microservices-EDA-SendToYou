c4a33766a934736bee051060fa039e295c185197
Alex <73026276+alex28042@users.noreply.github.com>
2023-08-26 19:36:48 +0200

    Update UserTest.java

--- package-micro-api/src/test/java/send/toyou/packagemicroapi/domain/UserTest.java
+++ package-micro-api/src/test/java/send/toyou/packagemicroapi/domain/UserTest.java
@@ -2,11 +2,14 @@ package send.toyou.packagemicroapi.domain;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import send.toyou.packagemicroapi.domain.persistence.User;
 
-public class User {
+public class UserTest {
     @Test
     void testUser() {
         var user = new User();
+        user.setId(23L);
+        user.setName("Test");
 
         Assertions.assertNotNull(user);
     }
