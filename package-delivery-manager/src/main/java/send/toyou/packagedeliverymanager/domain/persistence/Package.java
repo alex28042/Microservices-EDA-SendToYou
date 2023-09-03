5d5ae4d5270b40fe73f7fb9eacb5ea1d1ecceb4f
Alex <73026276+alex28042@users.noreply.github.com>
2023-09-03 13:11:44 +0200

    Adding Package Sent Processor

--- package-delivery-manager/src/main/java/send/toyou/packagedeliverymanager/domain/persistence/Package.java
+++ package-delivery-manager/src/main/java/send/toyou/packagedeliverymanager/domain/persistence/Package.java
@@ -4,6 +4,7 @@ import lombok.*;
 import org.springframework.data.relational.core.mapping.Table;
 import send.toyou.packagedeliverymanager.domain.enums.PackageStatusEnum;
 import send.toyou.packagedeliverymanager.domain.events.PackageProcessedEvent;
+import send.toyou.packagedeliverymanager.domain.events.PackageSentEvent;
 import send.toyou.packagedeliverymanager.domain.valueObjects.Address;
 
 import java.time.LocalDateTime;
@@ -32,6 +33,17 @@ public class Package {
             packageProcessedEvent.getName(),
             packageProcessedEvent.getStatus(),
             packageProcessedEvent.getDateCreated() == null ? null : packageProcessedEvent.getDateCreated().toString()
+        );
+    }
+
+    public static Package fromPackageSentEvent(PackageSentEvent packageSentEvent) {
+        return new Package(
+                packageSentEvent.getId(),
+                packageSentEvent.getSenderUserId(),
+                packageSentEvent.getReceipterUserId(),
+                packageSentEvent.getName(),
+                packageSentEvent.getStatus(),
+                packageSentEvent.getDateCreated() == null ? null : packageSentEvent.getDateCreated().toString()
         );
     }
 }
