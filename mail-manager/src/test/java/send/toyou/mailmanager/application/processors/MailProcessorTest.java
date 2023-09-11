33fa1a8ef65b403d563d26174fe0da5f02dbd74e
Alex <73026276+alex28042@users.noreply.github.com>
2023-09-11 21:52:10 +0200

    testing

--- mail-manager/src/test/java/send/toyou/mailmanager/application/processors/MailProcessorTest.java
+++ mail-manager/src/test/java/send/toyou/mailmanager/application/processors/MailProcessorTest.java
@@ -1,6 +1,7 @@
 package send.toyou.mailmanager.application.processors;
 
 import io.r2dbc.spi.ConnectionFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,9 +13,13 @@ import org.springframework.cloud.stream.binder.test.OutputDestination;
 import org.springframework.cloud.stream.binder.test.TestChannelBinderConfiguration;
 import org.springframework.context.annotation.Import;
 import org.springframework.core.io.Resource;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.r2dbc.connection.init.ScriptUtils;
 import org.springframework.test.context.ActiveProfiles;
 import reactor.core.publisher.Mono;
+import send.toyou.mailmanager.domain.enums.PackageStatusEnum;
+import send.toyou.mailmanager.domain.events.NewEmailPackageEvent;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -28,6 +33,8 @@ public class MailProcessorTest {
 
     @Autowired
     private InputDestination inputDestination;
+
+    private final int TIME_OUT = 5000;
 
     @BeforeEach
     void init(@Value("classpath:/schema.sql") Resource resource) {
@@ -48,5 +55,20 @@ public class MailProcessorTest {
     @Test
     void processorTest(@Value("classpath:/insert-data.sql") Resource resource) {
         this.executeScriptBlocking(resource);
+
+        var newEmailPackageEvent = new NewEmailPackageEvent();
+
+        newEmailPackageEvent.setEmailReceipter("fff");
+        newEmailPackageEvent.setPackageStatusEnum(PackageStatusEnum.PROCESSING);
+
+        Message<NewEmailPackageEvent> message = MessageBuilder.withPayload(newEmailPackageEvent).build();
+
+        this.inputDestination.send(message, "sendtoyou.new-email-event");
+
+        var result = this.outputDestination.receive(TIME_OUT, "sendtoyou.email-completed");
+
+        var emailCompleted = result.getPayload();
+
+        Assertions.assertThat(emailCompleted).isNotNull();
     }
 }
