f381c4c6bb11fd58932a6eb6f96b9c696620a523
Alex <73026276+alex28042@users.noreply.github.com>
2023-09-24 20:21:16 +0200

    api gateway

--- api-gateway/src/main/java/send/toyou/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/send/toyou/apigateway/ApiGatewayApplication.java
@@ -2,10 +2,10 @@ package send.toyou.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
